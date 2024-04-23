a55c459a1600697b20aa25e04bcb5a39edafc481
teresalee1231 <69709178+teresalee1231@users.noreply.github.com>
2024-04-23 13:54:15 -0700

    started the study page jsx

---
+++ my-react-app/src/pages/StudyPage.jsx
@@ -0,0 +1,15 @@
+import { BsCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
+import Flashcard from "../components/Flashcard";
+
+function StudyPage() {
+  return (
+    <>
+    <BsCaretLeftFill />
+    <BsFillCaretRightFill />
+  </>
+  )
+
+
+}
+
+export default StudyPage;
